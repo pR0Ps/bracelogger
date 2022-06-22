8bd816fc2629da2e669dcbd09a760d4e6e09bc3a
Carey Metcalfe <carey@cmetcalfe.ca>
2022-06-22 16:42:04 -0400

    Update CI and declare support for PyPy
     - Use newest version of actions/setup-python
     - Add testing on PyPy
     - Add testing on Python v3.11 betas
     - Re-add testing on Python v3.0-v3.3 (Windows only)

--- setup.py
+++ setup.py
@@ -36,6 +36,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
         "Intended Audience :: Developers",
         "Topic :: System :: Logging",
         "Operating System :: OS Independent",
