c055dee6eaa6e15b8b4e9391f68951b3eb34ee58
Carey Metcalfe <carey@cmetcalfe.ca>
2023-03-27 00:14:27 -0400

    Refactor method of patching
    Now uses mixin classes and wraps loggers instead of modifying them. This
    means that getting the same logger using `bracelogger.get_logger` and
    `logging.getLogger` and using different formats for them is now
    possible.
    
    It also means that `LogRecord` objects no longer have a `getMessage`
    attribute on them which would get confused with other extra attributes
    since that function is on the `BracelogRecordMixin` instead of the
    object.

--- bracelogger.py
+++ bracelogger.py
@@ -2,9 +2,7 @@
 
 """Use brace-style string formatting in log messages"""
 
-import functools
 import logging
-import types
 
 try:
     from collections.abc import Mapping
@@ -12,43 +10,68 @@ except ImportError:  # pragma: no cover
     from collections import Mapping
 
 
-def _get_message(record):
-    """Implement logging.LogRecord.getMessage using brace-style string formatting"""
-    msg = str(record.msg)
-    args = record.args
+_class_cache = {}
 
-    # The logging module has a special case where if a LogRecord is constructed
-    # with a single non-empty dict to format into the message, that single dict
-    # is treated as the args instead of the first in a tuple of args.
-    # This is done to enable using key-based templating in log messages. Ex:
-    #     logging.info("a:%(a)s, b:%(b)s", {"a": 1, "b": 2})
-    #
-    # This case is handled here by splatting the dict in as kwargs to allow
-    # using key-based format strings, but also passing it as the first arg to
-    # preserve compatibility with index-based format strings.
-    if isinstance(args, Mapping):
-        msg = msg.format(args, **args)
-    elif args:
-        msg = msg.format(*args)
-    return msg
+def _make_class(name, mixin, type_):
+    if type_ not in _class_cache:
+        _class_cache[type_] = type(name, (mixin, *type_.__mro__), {})
+    return _class_cache[type_]
 
 
-def _handle_wrap(fcn):
-    """Wrap logging.Handler.handle to use _getMessage"""
+class BracelogRecordMixin:
 
-    @functools.wraps(fcn)
-    def handle(record):
-        record.getMessage = types.MethodType(_get_message, record)
-        return fcn(record)
+    def getMessage(record):
+        """Implement logging.LogRecord.getMessage using brace-style string formatting"""
+        msg = str(record.msg)
+        args = record.args
 
-    return handle
+        # The logging module has a special case where if a LogRecord is constructed
+        # with a single non-empty dict to format into the message, that single dict
+        # is treated as the args instead of the first in a tuple of args.
+        # This is done to enable using key-based templating in log messages. Ex:
+        #     logging.info("a:%(a)s, b:%(b)s", {"a": 1, "b": 2})
+        #
+        # This case is handled here by splatting the dict in as kwargs to allow
+        # using key-based format strings, but also passing it as the first arg to
+        # preserve compatibility with index-based format strings.
+        if isinstance(args, Mapping):
+            msg = msg.format(args, **args)
+        elif args:
+            msg = msg.format(*args)
+        return msg
+
+
+class BraceloggerMixin:
+    """A logger wrapper that causes all handled LogRecords to use brace-style formatting"""
+
+    __log_functions__ = {"debug", "info", "warn", "warning", "error", "exception", "critical", "fatal"}
+
+    def __init__(self, logger):
+        self._wrapped = logger
+
+    def __getattribute__(self, key):
+        if key in type(self).__log_functions__ or key in {"_log", "_wrapped", "log", "handle", "getChild"}:
+            return super().__getattribute__(key)
+        return getattr(self._wrapped, key)
+
+    def __setattr__(self, key, val):
+        if key == "_wrapped":
+            return super().__setattr__(key, val)
+        return setattr(self._wrapped, key, val)
+
+    def __delattr__(self, key):
+        return delattr(self._wrapped, key)
+
+    def handle(self, record):
+        record.__class__ = _make_class("BracelogRecord", BracelogRecordMixin, type(record))
+        return super().handle(record)
+
+    def getChild(self, suffix):
+        """Get a Bracelogger which is a descendant to this one"""
+        logger = self._wrapped.getChild(suffix)
+        return _make_class("Bracelogger", BraceloggerMixin, type(logger))(logger)
 
 
 def get_logger(name=None):
-    """Get a logger instance that uses brace-style string formatting"""
-    log = logging.getLogger(name)
-    if not hasattr(log, "_bracelogger"):
-        log.handle = _handle_wrap(log.handle)
-    log._bracelogger = True
-
-    return log
+    logger = logging.getLogger(name)
+    return _make_class("Bracelogger", BraceloggerMixin, type(logger))(logger)
